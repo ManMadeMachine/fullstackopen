c8210d26f8372c33546b1e5fde7839db1f238588
Eetu <Eetu.Suonpaa@hotmail.com>
2019-12-04 00:01:27 +0200

    unicafe part 4

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -11,13 +11,60 @@ const Button = ({onClick, text}) => {
     );
 };
 
-const Statistic = ({label, count}) => <p>{label} {count}</p>;
+const Statistic = ({label, value, postFix}) => <p>{label} {value} {postFix}</p>;
+
+const Statistics = (props) => {
+    const {good, neutral, bad, total, average, positivePercentage} = props.stats;
+
+    if (good === 0 && neutral === 0 && bad === 0){
+        return(
+            <p>No feedback given</p>
+        );
+    }
+
+    return(
+        <div>
+            <Statistic label="good" value={good} />
+            <Statistic label="neutral" value={neutral} />
+            <Statistic label="bad" value={bad} />
+            <Statistic label="all" value={total} />
+            <Statistic label="average" value={average} />
+            <Statistic label="positive" value={positivePercentage} postFix="%" />
+        </div>
+    );
+};
+
 
 const App = () => {
     // save clicks of each button to own state
     const [good, setGood] = useState(0);
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
+
+    const total = () => good + neutral + bad;
+    const average = () => {
+        if (total() === 0){
+            return 0;
+        }
+        return (good - bad) / total();
+    };  
+
+    const positivePercentage = () => {
+        if (total() === 0){
+            return 0;
+        }
+
+        return (good / total()) * 100;
+    }
+
+    const stats = {
+        good,
+        neutral,
+        bad,
+        total: total(),
+        average: average(),
+        positivePercentage: positivePercentage()
+    }
 
     return (
         <div>
@@ -27,9 +74,7 @@ const App = () => {
             <Button onClick={() => setBad(bad + 1)} text="bad" />
 
             <Header text="statistics" />
-            <Statistic label="good" count={good} />
-            <Statistic label="neutral" count={neutral} />
-            <Statistic label="bad" count={bad} />
+            <Statistics stats={stats}/>
         </div>
     );
 }
