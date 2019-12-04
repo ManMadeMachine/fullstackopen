e293f0f911c67dac054d45c4eaabfa6216733a59
Eetu <Eetu.Suonpaa@hotmail.com>
2019-12-04 16:27:40 +0200

    stuff

--- playground/src/App.js
+++ playground/src/App.js
@@ -4,8 +4,13 @@ import Note from './components/Note';
 const App = (props) => {
     const [notes, setNotes] = useState(props.notes);
     const [newNote, setNewNote] = useState('');
+    const [showAll, setShowAll] = useState(true);
 
-    const rows = () => notes.map(note => <Note key={note.id} note={note} />);
+    const notesToShow = showAll
+        ? notes
+        : notes.filter(note => note.important === true);
+
+    const rows = () => notesToShow.map(note => <Note key={note.id} note={note} />);
 
     const addNote = (event) => {
         event.preventDefault();
@@ -27,6 +32,12 @@ const App = (props) => {
 
     return (
         <div>
+            <h1>Notes</h1>
+            <div>
+                <button onClick={() => setShowAll(!showAll)}>
+                    show {showAll ? 'important' : 'all'}
+                </button>
+            </div>
             <ul>
                 {rows()}
             </ul>
