b5cfca1ca126c357028b4d90c16282e25d490e70
Eetu Suonpää <Eetu.Suonpaa@insta.fi>
2019-12-05 16:56:02 +0200

    learning stuff

--- playground/src/App.js
+++ playground/src/App.js
@@ -18,25 +18,39 @@ const App = () => {
 
     useEffect(hook, []);
 
-    console.log('render', notes.length, 'notes');
-
     const notesToShow = showAll
         ? notes
         : notes.filter(note => note.important === true);
 
-    const rows = () => notesToShow.map(note => <Note key={note.id} note={note} />);
+    const toggleImportanceOf = id => {
+        console.log(`toggle importance of ${id}`)
+        const url = `http://localhost:3001/notes/${id}`;
+        const note = notes.find(n => n.id === id);
+        const changedNote = {...note, important: !note.important};
+
+        axios.put(url, changedNote).then(response => {
+            setNotes(notes.map(note => note.id !== id ? note : response.data));
+        });
+    };
+
+    const rows = () => notesToShow.map(note => <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)}/>);
 
     const addNote = (event) => {
         event.preventDefault();
         const noteObject = {
             content: newNote,
             date: new Date().toISOString,
-            important: Math.random() > 0.5,
-            id: notes.length + 1
+            important: Math.random() > 0.5
         };
 
-        setNotes(notes.concat(noteObject));
-        setNewNote('');
+        axios
+            .post('http://localhost:3001/notes', noteObject)
+            .then(response => {
+                console.log(response);
+                setNotes(notes.concat(response.data));
+                setNewNote('');
+            });
+
     };
 
     const handleNoteChange = (event) => {
