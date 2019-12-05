9077f5bf882158e3d3da3191f2dadc400284379a
Eetu <Eetu.Suonpaa@hotmail.com>
2019-12-05 02:58:38 +0200

    added filtering to phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,34 +2,67 @@ import React, {useState} from 'react';
 
 const App = () =>  {
   const [persons, setPersons] = useState([
-    {name: 'Arto Hellas'}
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState(''); 
+  const [searchFilter, setSearchFilter] = useState('');
 
   const nameChangeHandler = (event) => {
-    console.log("Change");
     setNewName(event.target.value);
   };
 
-  const addName = (event) => {
-    event.preventDefault();
-    setPersons(persons.concat({name: newName}));
-    setNewName('');
+  const numberChangeHandler = (event) => {
+    setNewNumber(event.target.value);
   };
 
+  const filterPersons = (event) => {
+    setSearchFilter(event.target.value);
+
+    console.log(searchFilter);
+  };
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const existingName = persons.find((person) => person.name === newName);
+    if (existingName){
+      alert(`${newName} already exists in the phonebook!`);
+      return;
+    }
+
+    setPersons(persons.concat({name: newName, number: newNumber}));
+    setNewName('');
+    setNewNumber('');
+  };
+
+  // forcing names to lowercase in filtering to make the filter case insensitive
+  const filteredPersons = persons.filter(person => 
+      person.name.toLowerCase().includes(searchFilter.toLowerCase())
+    );
+  
   const personRows = () => {
-    return persons.map(person => <p>{person.name}</p>)
+      return filteredPersons.map(person => <p key={person.name}>{person.name} {person.number}</p>)
   }
 
   return (
     <div>
       <h1>Phonebook</h1>
+      <div>
+        filter shown with <input onChange={filterPersons} value={searchFilter}/>
+      </div>
+      <h2>Add person</h2>
       <form>
         <div>
           name: <input onChange={nameChangeHandler} value={newName}/>
         </div>
         <div>
-          <button type="submit" onClick={addName}>add</button>
+          number: <input onChange={numberChangeHandler} value={newNumber}/>
+        </div>
+        <div>
+          <button type="submit" onClick={addPerson}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
