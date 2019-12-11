a2bacb18ce33113f04157b027adeb5a6ca50a4d0
Eetu <Eetu.Suonpaa@hotmail.com>
2019-12-11 04:10:05 +0200

    stuff

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,1 +1,10 @@
-console.log("hello World");
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hey');
+});
+
+const port = 3001;
+app.listen(port);
+console.log('Yeah');
